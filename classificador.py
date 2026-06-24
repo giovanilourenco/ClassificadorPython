8e60ee915031c29b2fc46c511ce7f43a07a6a0c1
Giovani <54555151+giovanilourenco@users.noreply.github.com>
2026-06-24 16:17:49 -0300

    Create classificador.py

---
+++ classificador.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+
+#Caminho que será organizado
+pasta = r"C:\Users\Cliente\Downloads"
+
+#Extensões
+tipos = {
+    "Imagens": [".jpg", ".jpeg", ".png", ".gif"],
+    "Documentos": [".pdf", ".docx", ".txt", ".xlsx", ".csv"],
+    "Videos": [".mp4", ".avi", ".mvk"]
+}
+
+#Percorre todos os arquivos disponíveis na pasta
+for arquivo in os.listdir(pasta):
+    caminho_arquivo = os.path.join(pasta, arquivo)
+
+    #Ignora pastas
+    if os.path.isfile(caminho_arquivo):
+        extensao = os.path.splitext(arquivo)[1].lower()
+
+        for categoria, extensoes in tipos.items():
+            if extensao in extensoes:
+                pasta_destino = os.path.join(pasta, categoria)
+
+                #Cria a pasta caso não exista
+                os.makedirs(pasta_destino, exist_ok=True)
+
+                #Move o arquivo
+                shutil.move(
+                    caminho_arquivo,
+                    os.path.join(pasta_destino, arquivo)
+                )
+
+                print(f"{arquivo} -> {categoria}")
+
+                break
+print("Organizado!")
